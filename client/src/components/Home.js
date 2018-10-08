483b3e3cc1967d19820cb56c4de55cc4ffa40df4
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-10-08 10:30:37 -0600

    configured Cors

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react';
+import axios from 'axios'
 
 class Home extends Component {
+  state = { coin: {} }
+
+  clicky = () => {
+    axios.post('api/coins?coin?coin=btc')
+      .then ( res => { debugger })
+  }
+
+  clicky2 = ()  => { axios.get('api/coins')
+  .then ( res => { debugger })
+}
+
+
   render() {
     return (
-      <Header as="h1" textAlign="center">Home Component</Header>
-    );
+      <div>
+<Button onClick={this.clicky}>
+      Click Me!
+</Button>   
+ <Button onClick={this.clicky2}>
+      Click Me!
+</Button>   
+</div>
+ )
   }
 }
 
